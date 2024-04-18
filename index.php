c32dedcc8687fae9dfccc01a105ecea496392e44
mdaashir <s.mohamedaashir@gmail.com>
2024-04-18 06:29:21 +0530

    Update .gitignore, composer.json and add favicons to index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+	<head>
+		<!-- Favicon -->
+		<link href="img/restaurant/favicon.ico" rel="icon">
+		<link rel="apple-touch-icon" sizes="180x180" href="img/restaurant/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="img/restaurant/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="img/restaurant/favicon-16x16.png">
+		<link rel="manifest" href="img/restaurant/site.webmanifest">
+	</head>
 	<body>
 		<?php
 			echo "My first PHP script!";
